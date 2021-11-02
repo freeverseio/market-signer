4d84f114f4ae20d9c74314ccf5f383b758ae1679
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2021-11-02 15:39:56 +0100

    removed unuseful line from doc

--- src/b2bAuthentication/token.js
+++ src/b2bAuthentication/token.js
@@ -1,5 +1,4 @@
 // Copyright (c) 2021 Freeverse.io <dev@freeverse.io>
-// Library for signing API calls to Living Assets market
 
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
