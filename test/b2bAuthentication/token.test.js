7d1556caedf3bbf1e55fc3f549287b0dd8115f1b
Carla Trujillo Santiago <cartrujillosa@gmail.com>
2021-12-20 12:07:00 +0100

    fix tests

--- test/b2bAuthentication/token.test.js
+++ test/b2bAuthentication/token.test.js
@@ -1,18 +1,18 @@
 const { expect } = require('chai');
-const { sign, verify } = require('../../src/b2bAuthentication/token');
+const { getToken, verifyToken } = require('../../src/b2bAuthentication/token');
 
 describe('b2bAuthentication', () => {
   const pvk = 'c6d398e89bf7cbda7663ca881bd992eb80ad170e4ca0bd65a8b1c719ee02bc67';
 
-  it('sign', () => {
-    const token = sign({ pvk, time: 40 });
+  it('getToken', () => {
+    const token = getToken({ pvk, time: 40 });
     expect(token).equal('40:/BMyyMjBrQPF86Y2kRBWxHd4HPXCWRxuMMc5q6n44NEjQQrM0W7csM+wkTbOQvH6pRlxHp8bV9CIpnCuwcD5Zxs=');
   });
 
-  it('verify', () => {
+  it('verifyToken', () => {
     const time = 40;
     const epsilon = 1;
-    const decoded = verify({
+    const decoded = verifyToken({
       token: '40:/BMyyMjBrQPF86Y2kRBWxHd4HPXCWRxuMMc5q6n44NEjQQrM0W7csM+wkTbOQvH6pRlxHp8bV9CIpnCuwcD5Zxs=',
       time,
       epsilon,
@@ -22,36 +22,36 @@ describe('b2bAuthentication', () => {
     expect(decoded.address).equal('0x426dbD2b27A3C53be05EBCf30354D86cee848d65');
   });
 
-  it('sing and verify', () => {
+  it('sing and verifyToken', () => {
     const epsilon = 0;
     const time = 50;
-    const token = sign({ pvk, time });
-    const decoded = verify({ token, time, epsilon });
+    const token = getToken({ pvk, time });
+    const decoded = verifyToken({ token, time, epsilon });
     expect(decoded.address).equal('0x426dbD2b27A3C53be05EBCf30354D86cee848d65');
   });
 
-  it('sing and verify fails', () => {
+  it('sing and verifyToken fails', () => {
     const epsilon = 0;
     const time = 50;
-    const token = sign({ pvk, time });
-    expect(() => verify({ token, time: time + 1, epsilon })).to.throw('token out of time -1, epsilon 0');
+    const token = getToken({ pvk, time });
+    expect(() => verifyToken({ token, time: time + 1, epsilon })).to.throw('token out of time -1, epsilon 0');
   });
 
   it('check epsilon', () => {
     const epsilon = 1;
     const time = 50;
-    const token = sign({ pvk, time });
-    expect(() => verify({ token, time: time + 1, epsilon })).to.not.throw();
-    expect(() => verify({ token, time: time - 1, epsilon })).to.not.throw();
-    expect(() => verify({ token, time: time - 2, epsilon })).to.throw();
-    expect(() => verify({ token, time: time + 2, epsilon })).to.throw();
+    const token = getToken({ pvk, time });
+    expect(() => verifyToken({ token, time: time + 1, epsilon })).to.not.throw();
+    expect(() => verifyToken({ token, time: time - 1, epsilon })).to.not.throw();
+    expect(() => verifyToken({ token, time: time - 2, epsilon })).to.throw();
+    expect(() => verifyToken({ token, time: time + 2, epsilon })).to.throw();
   });
 
-  it('verify malformed token', () => {
-    expect(() => verify({ token: '40:/BMyyMjBrQPF86Y2kRBWxHd4HPXCWRxuMMc5q6n44NEjQQrM0W7csM+wkTbOQvH6pRlxHp8bV9CIpnCuwcD5Zxs=', time: 40, epsilon: 0 })).to.not.throw();
-    expect(() => verify({ token: '40,/BMyyMjBrQPF86Y2kRBWxHd4HPXCWRxuMMc5q6n44NEjQQrM0W7csM+wkTbOQvH6pRlxHp8bV9CIpnCuwcD5Zxs=', time: 40, epsilon: 0 })).to.throw();
-    expect(() => verify({ token: '40:/BMyy:MjBrQPF86Y2kRBWxHd4HPXCWRxuMMc5q6n44NEjQQrM0W7csM+wkTbOQvH6pRlxHp8bV9CIpnCuwcD5Zxs=', time: 40, epsilon: 0 })).to.throw();
-    expect(() => verify({ token: ':/BMyyMjBrQPF86Y2kRBWxHd4HPXCWRxuMMc5q6n44NEjQQrM0W7csM+wkTbOQvH6pRlxHp8bV9CIpnCuwcD5Zxs=', time: 40, epsilon: 0 })).to.throw();
-    expect(() => verify({ token: ':', time: 40, epsilon: 0 })).to.throw();
+  it('verifyToken malformed token', () => {
+    expect(() => verifyToken({ token: '40:/BMyyMjBrQPF86Y2kRBWxHd4HPXCWRxuMMc5q6n44NEjQQrM0W7csM+wkTbOQvH6pRlxHp8bV9CIpnCuwcD5Zxs=', time: 40, epsilon: 0 })).to.not.throw();
+    expect(() => verifyToken({ token: '40,/BMyyMjBrQPF86Y2kRBWxHd4HPXCWRxuMMc5q6n44NEjQQrM0W7csM+wkTbOQvH6pRlxHp8bV9CIpnCuwcD5Zxs=', time: 40, epsilon: 0 })).to.throw();
+    expect(() => verifyToken({ token: '40:/BMyy:MjBrQPF86Y2kRBWxHd4HPXCWRxuMMc5q6n44NEjQQrM0W7csM+wkTbOQvH6pRlxHp8bV9CIpnCuwcD5Zxs=', time: 40, epsilon: 0 })).to.throw();
+    expect(() => verifyToken({ token: ':/BMyyMjBrQPF86Y2kRBWxHd4HPXCWRxuMMc5q6n44NEjQQrM0W7csM+wkTbOQvH6pRlxHp8bV9CIpnCuwcD5Zxs=', time: 40, epsilon: 0 })).to.throw();
+    expect(() => verifyToken({ token: ':', time: 40, epsilon: 0 })).to.throw();
   });
 });
