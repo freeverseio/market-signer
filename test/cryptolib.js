10dba7198769ddaa46631e4a48058d27623c55e8
Toni Mateos <tmateos@freeverse.io>
2022-01-17 12:19:54 +0100

    adding payments contract deploy in test

--- test/cryptolib.js
+++ test/cryptolib.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 const Eth = require('web3-eth');
 const Contract = require('web3-eth-contract');
 const myTokenJSON = require('../src/contracts/MyToken.json');
+const PaymentsJSON = require('../src/contracts/PaymentsERC20.json');
 const pvk = 'F2F48EE19680706196E2E339E5DA3491186E0C4C5030670656B0E0164837257D';
 
 const account = new Accounts().privateKeyToAccount(pvk);
@@ -25,27 +26,52 @@ const logger = {
 const ganacheSubprovider = new GanacheSubprovider({
   logger,
   verbose: false,
-  accounts: [{ balance: '4500000000000000000000' }, { balance: '0x0' }],
+  accounts: [{ balance: '4500000000000000000000000' }, { balance: '0x0' }],
   port: configs.port,
   networkId: configs.networkId,
   mnemonic: configs.mnemonic,
 });
 
-let provider;
-beforeEach(async () => {
-  provider = new Web3ProviderEngine();
-});
+const name = 'TokenName';
+const symbol = 'TokenSymbol';
+const currencyDescriptor = 'TestERC20Token';
 
-it('deploys contract', async () => {
-  provider.addProvider(ganacheSubprovider);
-  provider.start();
-  Contract.setProvider(provider);
-  const contract = new Contract(myTokenJSON.abi);
-  const name = 'TokenName';
-  const symbol = 'TokenSymbol';
-  const deploy = await contract.deploy({ data: myTokenJSON.bytecode, arguments: [name, symbol] });
-  const sent = await deploy.send({ from: account.address, gas: 1500000, gasPrice: '30000000000000' });
-  const contractAddr = sent.options.address;
-  assert.equal(contractAddr === undefined, false);
-  provider.stop();
+let provider;
+let erc20Contract;
+let erc20Deploy;
+let erc20Addr;
+let paymentsContract;
+let paymentsDeploy;
+let paymentsAddr;
+
+describe('Payments in ERC20', () => {
+
+  beforeEach(async () => {
+    provider = new Web3ProviderEngine();
+    provider.addProvider(ganacheSubprovider);
+    provider.start();
+    Contract.setProvider(provider);
+    // deploy MyToken ERC20
+    erc20Contract = new Contract(myTokenJSON.abi);
+    erc20Deploy = await erc20Contract.deploy({
+      data: myTokenJSON.bytecode,
+      arguments: [name, symbol],
+    }).send({ from: account.address, gas: 1500000, gasPrice: '30000000000000' });
+    erc20Addr = erc20Deploy.options.address;
+    // deploy Payments contract
+    paymentsContract = new Contract(PaymentsJSON.abi);
+    paymentsDeploy = await paymentsContract.deploy({
+      data: PaymentsJSON.bytecode,
+      arguments: [erc20Addr, currencyDescriptor],
+    }).send({ from: account.address, gas: 5000000, gasPrice: '3000000000000' });
+    paymentsAddr = paymentsDeploy.options.address;
+  });
+  afterEach(async () => {
+    provider.stop();
+  });
+
+  it('deploys contracts', async () => {
+    assert.equal(erc20Addr === undefined, false);
+    assert.equal(paymentsAddr === undefined, false);
+  });
 });
